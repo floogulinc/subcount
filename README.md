e4f9e6041855aac5f7f6d6448ecb73579e285dae
Paul Friederichsen <floogulinc@gmail.com>
2021-05-16 00:26:33 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+subcount
+========
+
+Count your subscriptions on Patreon and Fanbox
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/subcount.svg)](https://npmjs.org/package/subcount)
+[![Downloads/week](https://img.shields.io/npm/dw/subcount.svg)](https://npmjs.org/package/subcount)
+[![License](https://img.shields.io/npm/l/subcount.svg)](https://github.com/floogulinc/subcount/blob/master/package.json)
+
+<!-- toc -->
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
+# Usage
+<!-- usage -->
+```sh-session
+$ npm install -g subcount
+$ subcount COMMAND
+running command...
+$ subcount (-v|--version|version)
+subcount/0.0.0 win32-x64 node-v12.14.0
+$ subcount --help [COMMAND]
+USAGE
+  $ subcount COMMAND
+...
+```
+<!-- usagestop -->
+# Commands
+<!-- commands -->
+
+<!-- commandsstop -->
---
+++ src/index.ts
@@ -0,0 +1,206 @@
+import {Command, flags} from '@oclif/command'
+import * as Parser from '@oclif/parser'
+import axios from 'axios'
+import cli from 'cli-ux'
+
+interface PatreonPledgesResp {
+  data: {
+    attributes: {
+      amount_cents: number;
+      created_at: string;
+      currency: string;
+    };
+    id: string;
+    relationships: {
+      campaign: {
+        data: {
+          id: string;
+          type: string;
+        };
+        links: {
+          related: string;
+        };
+      };
+    };
+  }[];
+  included: {
+    attributes: {
+      currency: string;
+      creation_name: string;
+      is_charge_upfront: boolean;
+      is_charged_immediately: boolean;
+      is_monthly: boolean;
+      name: string;
+      one_liner: string;
+      outstanding_payment_amount_cents: number;
+      patron_count: number;
+      pay_per_name: string;
+      pledge_sum: number;
+      pledge_sum_currency: string;
+      published_at: string;
+      summary: string;
+      url: string;
+    };
+    id: string;
+    type: string;
+  }[];
+}
+
+interface FanboxSupportingResp {
+  body: {
+    coverImageUrl: string;
+    creatorId: string;
+    description: string;
+    fee: number;
+    hasAdultContent: boolean;
+    id: string;
+    paymentMethod: string;
+    title: string;
+    user: {
+      iconUrl: string;
+      name: string;
+      userId: string;
+    };
+  }[];
+}
+
+class Subcount extends Command {
+  static description = 'describe the command here'
+
+  static flags = {
+    // add --version flag to show CLI version
+    version: flags.version({char: 'v'}),
+    help: flags.help({char: 'h'}),
+
+    fanbox: flags.boolean({char: 'f'}),
+    patreon: flags.boolean({char: 'p'}),
+    ...cli.table.flags()
+  }
+
+  static args: Parser.args.Input = [
+    {
+      name: 'apikey',
+      required: true
+    },
+    {
+      name: 'apiurl',
+      default: 'http://localhost:45869'
+    },
+  ]
+
+  async run() {
+    const {args, flags} = this.parse(Subcount)
+    if (flags.patreon) {
+      await this.getPatreon(args.apiurl, args.apikey);
+    }
+    if (flags.fanbox) {
+      await this.getFanbox(args.apiurl, args.apikey);
+    }
+  }
+
+  async getPatreon(apiUrl: string, apiKey: string) {
+    cli.action.start('Checking Patreon');
+    const cookies = await this.getCookies(apiUrl, apiKey, 'patreon.com');
+    const patreonResp = await axios.get<PatreonPledgesResp>('https://www.patreon.com/api/pledges', {
+      headers: {
+        Cookie: cookies
+      },
+      params: {
+        include: 'campaign',
+        'json-api-use-default-includes': 'false'
+      }
+    })
+    const patreonData = patreonResp.data;
+
+    const patreonPairs = patreonData.data.map(data => {
+      const campaign = patreonData.included.find(c => c.id === data.relationships.campaign.data.id);
+
+      return {
+        pledge: {
+          ...data.attributes,
+          id: data.id
+        },
+        campaign: {
+          name: campaign?.attributes.name,
+          id: campaign?.id
+        }
+      };
+    });
+
+    const tableData = patreonPairs.map(({pledge, campaign}) => ({
+      name: campaign.name,
+      pledge: `${pledge.amount_cents / 100} ${pledge.currency}`
+    }));
+
+    cli.action.stop();
+
+    cli.table(tableData, {
+      name: {
+
+      },
+      pledge: {
+
+      },
+    }, {
+      printLine: this.log,
+      ...flags, // parsed flags
+    });
+
+    const patreonTotal = patreonPairs.map(p => p.pledge.amount_cents).reduce((p, c) => p + c, 0);
+
+    this.log(`Patreon total: ${patreonTotal / 100}`);
+    this.log('');
+  }
+
+  async getFanbox(apiUrl: string, apiKey: string) {
+    cli.action.start('Checking Fanbox');
+    const cookies = await this.getCookies(apiUrl, apiKey, 'fanbox.cc');
+    const fanboxResp = await axios.get<FanboxSupportingResp>('https://api.fanbox.cc/plan.listSupporting', {
+      headers: {
+        Cookie: cookies,
+        referer: 'https://www.fanbox.cc/',
+        origin: 'https://www.fanbox.cc'
+      },
+    })
+    const fanboxData = fanboxResp.data.body;
+
+    const tableData = fanboxData.map(pledge => ({
+      name: pledge.user.name,
+      pledge: `${pledge.fee} JPY`,
+    }));
+
+    cli.action.stop();
+
+    cli.table(tableData, {
+      name: {
+
+      },
+      pledge: {
+
+      },
+    }, {
+      printLine: this.log,
+      ...flags, // parsed flags
+    });
+
+    const fanboxTotal = fanboxData.map(p => p.fee).reduce((p, c) => p + c, 0);
+
+    this.log(`Fanbox total: ${fanboxTotal} JPY`);
+    this.log('');
+  }
+
+  async getCookies(apiUrl: string, apiKey: string, domain: string) {
+    const hydrusResponse = await axios.get(apiUrl + '/manage_cookies/get_cookies', {
+      params: {
+        domain
+      },
+      headers: {
+        'Hydrus-Client-API-Access-Key': apiKey
+      }
+    })
+    const cookies: string[][] = hydrusResponse.data.cookies;
+    return cookies.map(cookie => `${cookie[0]}=${cookie[1]}`).join('; ');
+  }
+}
+
+export = Subcount
